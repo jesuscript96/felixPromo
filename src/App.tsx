2e8fa23fcab3a37b0838a1d04d0b6bbcdf9f22cd
jesuscript96 <jvalenzuela.chulia@gmail.com>
2026-05-20 11:57:58 -0600

    orden

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Hero from './components/Hero';
 import FloatingNav from './components/FloatingNav';
 import AboutSection from './components/AboutSection';
 import ProjectSection from './components/ProjectSection';
+import AmenitiesSection from './components/AmenitiesSection';
+import DetailsSection from './components/DetailsSection';
 import PropertyDetails from './components/PropertyDetails';
 import Footer from './components/Footer';
 
@@ -31,9 +33,11 @@ function AppContent() {
     <div className="font-sans bg-brand-bg text-brand-text selection:bg-brand-accent selection:text-white">
       <FloatingNav />
       <Hero />
-      <ProjectSection />
-      <PropertyDetails />
       <AboutSection />
+      <ProjectSection />
+      <AmenitiesSection />
+      <DetailsSection />
+      <PropertyDetails />
       <Footer />
     </div>
   );
